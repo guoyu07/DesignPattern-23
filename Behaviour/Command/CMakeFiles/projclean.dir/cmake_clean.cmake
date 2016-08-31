d9c3b300122d02c1f9fcbc9da2e6ccda94cbe9bd
Thomas Rothe <eingefuchst@gmail.com>
2016-08-31 10:10:42 +0800

    deleted temps, added gitignores

--- Behaviour/Command/CMakeFiles/projclean.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/projclean"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/projclean.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
